598aa90cce29be84c236b66e715ed2e1b8511093
Derek S. Ray <derek@nineball.org>
2007-05-22 14:42:26 +0000

    git-svn-id: svn://sporkhack.com/nh@6 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- include/mondata.h
+++ include/mondata.h
@@ -127,6 +127,7 @@
 				 ((ptr) == &mons[PM_LONG_WORM]) || \
 				 ((ptr) == &mons[PM_LONG_WORM_TAIL]))
 #define is_covetous(ptr)	((ptr->mflags3 & M3_COVETOUS))
+#define is_skittish(ptr)   ((ptr->mflags3 & M3_SKITTISH))
 #define infravision(ptr)	((ptr->mflags3 & M3_INFRAVISION))
 #define infravisible(ptr)	((ptr->mflags3 & M3_INFRAVISIBLE))
 #define is_mplayer(ptr)		(((ptr) >= &mons[PM_ARCHEOLOGIST]) && \
