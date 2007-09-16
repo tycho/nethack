f3d929038b9155d489b631203b3cc3f2b995ee53
derek <derek@0b9a868d-f130-0410-bc2a-adc7ae8015a6>
2007-09-16 15:03:54 +0000

    Monsters that are bigger than your pets will attack your pets to get to you. Berserk monsters will attack _anything_ to get to you. (Most monsters will incidentally stomp on insects or MZ_TINY in their way.)
    git-svn-id: svn://sporkhack.com/nh@225 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- include/mondata.h
+++ include/mondata.h
@@ -132,6 +132,7 @@
 #define is_covetous(ptr)	((ptr->mflags3 & M3_COVETOUS))
 #define is_skittish(ptr)   ((ptr->mflags3 & M3_SKITTISH))
 #define is_accurate(ptr)   ((ptr->mflags3 & M3_EAGLEEYE))
+#define is_berserker(ptr)  ((ptr->mflags3 & M3_BERSERK))
 #define infravision(ptr)	((ptr->mflags3 & M3_INFRAVISION))
 #define infravisible(ptr)	((ptr->mflags3 & M3_INFRAVISIBLE))
 #define is_mplayer(ptr)		(((ptr) >= &mons[PM_ARCHEOLOGIST]) && \
