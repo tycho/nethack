8b02b010825971609d5940bb09b4d52cdbec9291
derek <derek@0b9a868d-f130-0410-bc2a-adc7ae8015a6>
2007-12-01 16:28:17 +0000

    Barbarian spell increased so he has some hope of casting it
    git-svn-id: svn://sporkhack.com/nh@321 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- src/role.c
+++ src/role.c
@@ -75,7 +75,7 @@ const struct Role roles[] = {
 	/* Init   Lower  Higher */
 	{ 14, 0,  0,10,  2, 0 },	/* Hit points */
 	{  1, 0,  0, 1,  0, 1 },10,	/* Energy */
-	10, 14, 0, 0,  8, A_INT, SPE_HASTE_SELF,      -4
+	10, 14, 0, 0,  8, A_INT, SPE_HASTE_SELF,      -10	/* this isn't accidental */
 },
 {	{"Caveman", "Cavewoman"}, {
 	{"Troglodyte",  0},
