42b24fe685840324444fb5c6ea8c7a3fe0585a16
Steven Noonan <steven@uplinklabs.net>
2009-04-21 02:34:18 -0700

    Statuscolors 1.0
    Customizable coloring for the status (bottom) lines
    Statuscolors is a patch for Nethack (version 3.4.3) that attempts to generalize
    the hpmon patch to be more like the menucolors patch. Unlike menucolors, it
    does not use regexps. Instead, it provides the following options:
    
    To enable statuscolors:
    OPTIONS=statuscolors
    
    To specify statuscolor options, write:
    STATUSCOLOR=<option>,<option>
    
    Numeric options have the format <field>%<max-percent>:<color-option>. For
    example:
    STATUSCOLOR=hp%15:red&bold,pw%100=green
    
    Text options have the format <text>:<color-option>. Text is case-insensitive.
    For example:
    STATUSCOLOR=hallu:orange,foodpois:red&inverse&blink
    
    A color option is a <color> followed by an optional sequence of &<attr>. Color
    and attribute names are case insensitive. Valid colors are:
    black blue brown cyan gray green lightblue lightcyan lightgreen
    lightmagenta magenta none orange red white yellow
    
    Valid attributes are:
    blink bold dim inverse none underline
    
    A reasonable set of defaults might be:
    STATUSCOLOR=hp%100=green,hp%66=yellow,hp%50=orange
    STATUSCOLOR=hp%33=red&bold,hp%15:red&inverse,hp%0:red&inverse&blink
    STATUSCOLOR=pw%100=green,pw%66=yellow,pw%50:orange,pw%33=red&bold
    STATUSCOLOR=burdened:yellow,stressed:orange,strained:red&bold
    STATUSCOLOR=overtaxed:red&inverse,overloaded:red&inverse&blink
    STATUSCOLOR=satiated:yellow,hungry:orange,weak:red&bold
    STATUSCOLOR=fainting:red&inverse,fainted:red&inverse&blink
    STATUSCOLOR=hallu:yellow,conf:orange,stun:red&bold
    STATUSCOLOR=ill:red&inverse,foodpois:red&inverse,slime:red&inverse
    STATUSCOLOR=held:red&inverse,blind:red&inverse
    
    Signed-off-by: Shachaf <shachaf+nethack@gmail.com>
    Signed-off-by: Oren Ben-Kiki <nethack-oren@ben-kiki.org>
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- include/config.h
+++ include/config.h
@@ -370,6 +370,8 @@ typedef unsigned char	uchar;
  */
 #endif
 
+#define STATUS_COLORS
+
 /*#define GOLDOBJ */	/* Gold is kept on obj chains - Helge Hafting */
 /*#define AUTOPICKUP_EXCEPTIONS */ /* exceptions to autopickup */
 
