a576f115773091fc92442a324dbaec8a737b7f9c
paxed <paxed@0b9a868d-f130-0410-bc2a-adc7ae8015a6>
2009-09-29 15:25:23 +0000

    lev_comp: New level flag, "graveyard"; this was previously hardcoded.
    git-svn-id: svn://sporkhack.com/nh@551 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- src/mkmaze.c
+++ src/mkmaze.c
@@ -463,11 +463,6 @@ fixup_special()
 		if (!rn2(3) && !is_pool(x,y))
 		    (void)maketrap(x, y, rn2(3) ? LANDMINE : SPIKED_PIT);
 	    }
-    } else if (Role_if(PM_PRIEST) && In_quest(&u.uz)) {
-	/* less chance for undead corpses (lured from lower morgues) */
-	level.flags.graveyard = 1;
-    } else if (Is_stronghold(&u.uz)) {
-	level.flags.graveyard = 1;
     } else if(Is_sanctum(&u.uz)) {
 	croom = search_special(TEMPLE);
 
