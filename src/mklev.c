caca7589c22e96e4f1c0f178d9c93db6be2a8b99
paxed <paxed@0b9a868d-f130-0410-bc2a-adc7ae8015a6>
2009-11-21 19:15:22 +0000

    Inaccessible niches occasionally have iron bars.
    git-svn-id: svn://sporkhack.com/nh@616 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- src/mklev.c
+++ src/mklev.c
@@ -503,6 +503,8 @@ int trap_type;
 		if(rn2(7))
 		    dosdoor(xx, yy, aroom, rn2(5) ? SDOOR : DOOR);
 		else {
+		    if (!rn2(2) && IS_WALL(levl[xx][yy].typ))
+			levl[xx][yy].typ = IRONBARS;
 		    if (!level.flags.noteleport)
 			(void) mksobj_at(SCR_TELEPORTATION,
 					 xx, yy+dy, TRUE, FALSE);
