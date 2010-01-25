d5b7443a34e0e3bb774702e03a41bde05c58645c
Steven Noonan <steven@uplinklabs.net>
2010-01-24 18:31:55 -0800

    disable external compression
    It's not necessary, and determining the path of gzip/compress is annoying. If
    we get it wrong, the NetHack thread starts spinning at 100% CPU constantly.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- include/config.h
+++ include/config.h
@@ -168,11 +168,11 @@
 
 #ifdef UNIX
 /* path and file name extension for compression program */
-#define COMPRESS "/usr/bin/compress"	/* Lempel-Ziv compression */
-#define COMPRESS_EXTENSION ".Z"		/* compress's extension */
+/*#define COMPRESS "/usr/bin/compress"	*/	/* Lempel-Ziv compression */
+/*#define COMPRESS_EXTENSION ".Z"	*/	/* compress's extension */
 /* An example of one alternative you might want to use: */
-/* #define COMPRESS "/usr/local/bin/gzip" */	/* FSF gzip compression */
-/* #define COMPRESS_EXTENSION ".gz" */		/* normal gzip extension */
+/* #define COMPRESS "/bin/gzip"		*/	/* FSF gzip compression */
+/* #define COMPRESS_EXTENSION ".gz"	*/	/* normal gzip extension */
 #endif
 
 #ifndef COMPRESS
