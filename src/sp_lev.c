e503b3d447fe9ba8583c25e83e4f6744947bce1c
Pasi Kallinen <paxed@alt.org>
2010-03-30 16:23:14 +0000

    Allow every level statement within object containers. Most won't make sense, but this simplifies the code and allows all flow controls in container definitions too.
    git-svn-id: svn://sporkhack.com/nh@658 0b9a868d-f130-0410-bc2a-adc7ae8015a6

--- src/sp_lev.c
+++ src/sp_lev.c
@@ -3052,7 +3052,7 @@ spo_object(coder)
     int nparams = 0;
 
     struct opvar *varparam;
-    struct opvar *id, *x, *y, *class, *containment;
+    struct opvar *id, *class, *containment;
 
     object tmpobj;
 
@@ -3070,12 +3070,11 @@ spo_object(coder)
     tmpobj.invis = 0;
     tmpobj.greased = 0;
     tmpobj.broken = 0;
+    tmpobj.x = tmpobj.y = -MAX_REGISTERS-1;
 
     if (!OV_pop_i(containment) ||
 	!OV_pop_i(id) ||
-	!OV_pop_i(class) ||
-	!OV_pop_i(y) ||
-	!OV_pop_i(x)) return;
+	!OV_pop_i(class)) return;
 
     if (!OV_pop_i(varparam)) return;
 
@@ -3142,6 +3141,13 @@ spo_object(coder)
 	    if (OV_typ(parm) == SPOVAR_INT)
 		tmpobj.broken = OV_i(parm);
 	    break;
+	case SP_O_V_COORD:
+	    if (OV_typ(parm) == SPOVAR_INT)
+		tmpobj.y = OV_i(parm);
+	    parm = splev_stack_pop(coder->stack);
+	    if (OV_typ(parm) == SPOVAR_INT)
+		tmpobj.x = OV_i(parm);
+	    break;
 	case SP_O_V_END:
 	    nparams = SP_O_V_END+1;
 	    break;
@@ -3157,8 +3163,6 @@ spo_object(coder)
     }
 
     tmpobj.id = OV_i(id);
-    tmpobj.x = OV_i(x);
-    tmpobj.y = OV_i(y);
     tmpobj.class = OV_i(class);
     tmpobj.containment = OV_i(containment);
 
@@ -3168,8 +3172,6 @@ spo_object(coder)
 
     opvar_free(varparam);
     opvar_free(id);
-    opvar_free(x);
-    opvar_free(y);
     opvar_free(class);
     opvar_free(containment);
 }
