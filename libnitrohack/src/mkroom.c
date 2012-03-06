391aea613bdcd165e21723d60085b2a9d4ccca2c
Tung Nguyen <tunginobi@gmail.com>
2012-03-06 16:29:13 +1100

    CS49: Add snow ants from SLASH'EM
    Identical to fire ants, but cold.

--- libnitrohack/src/mkroom.c
+++ libnitrohack/src/mkroom.c
@@ -350,10 +350,11 @@ static const struct permonst *antholemon(const d_level *dlev)
 	int mtyp;
 
 	/* Same monsters within a level, different ones between levels */
-	switch ((level_difficulty(dlev) + ((long)u.ubirthday)) % 3) {
+	switch ((level_difficulty(dlev) + ((long)u.ubirthday)) % 4) {
 	default:	mtyp = PM_GIANT_ANT; break;
 	case 0:		mtyp = PM_SOLDIER_ANT; break;
 	case 1:		mtyp = PM_FIRE_ANT; break;
+	case 2:		mtyp = PM_SNOW_ANT; break;
 	}
 	return (mvitals[mtyp].mvflags & G_GONE) ? NULL : &mons[mtyp];
 }
