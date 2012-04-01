d5592354d03a45a596c637d58311f2d8fa2a5fe1
Tung Nguyen <tunginobi@gmail.com>
2012-04-01 18:02:08 +1000

    CS88: Engravings from SLASH'EM

--- libnitrohack/src/engrave.c
+++ libnitrohack/src/engrave.c
@@ -26,6 +26,19 @@ static const char *const random_mesg[] = {
 	"Two thumbs up!", /* Siskel & Ebert */
 	"Hello, World!", /* The First C Program */
 	"As if!", /* Clueless */
+	/* From Slash'Em */
+	/* [Tom] added these */
+	"Y?v?l s??ks!", /* just kidding... */
+	"T?m ?as h?r?",
+	/* Tsanth added these */
+	"Gazortenplatz", /* Tribute to David Fizz */
+	"John 3:16", /* You see this everywhere; why not here? */
+	"....TCELES B HSUP   A magic spell?", /* Final Fantasy I (US) */
+	"Turn around.", /* Various people at various times in history */
+	"UY WUZ HERE", /* :] */
+	"I'll be back.", /* Terminator */
+	"That is not dead which can eternal lie.", /* HPL */
+	/* From UnNetHack */
 	"She's watching you!", /* Portal */
 	"The cake is a lie!", /* Portal */
 };
