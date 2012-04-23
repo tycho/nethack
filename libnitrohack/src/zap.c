cc8aa7bfd0cf2458e1684ab5e55cf9a4ed66b008
Tung Nguyen <tunginobi@gmail.com>
2012-04-23 22:39:37 +1000

    CS157: Raise chance of cursed wands blowing up
    1 in 30 instead of 1 in 100 - such a wand has a 50% chance of blowing up
    if zapped 20 times.

--- libnitrohack/src/zap.c
+++ libnitrohack/src/zap.c
@@ -1804,7 +1804,8 @@ int dozap(struct obj *obj)
 
 	/* zappable addition done by GAN 11/03/86 */
 	if (!zappable(obj)) pline("Nothing happens.");
-	else if (obj->cursed && !rn2(100)) {
+	/* PM 2008-04-16: 50% chance of blowing up, if zapped 20 times */
+	else if (obj->cursed && !rn2(30)) {
 		backfire(obj);	/* the wand blows up in your face! */
 		exercise(A_STR, FALSE);
 		return 1;
