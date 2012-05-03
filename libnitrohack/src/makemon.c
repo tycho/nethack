152f704be2a7d4e3a80d3b2cc4f910af8542be78
Tung Nguyen <tungtn3@gmail.com>
2012-05-03 17:55:57 +1000

    CS284: Give Cthulhu an amulet of reflection

--- libnitrohack/src/makemon.c
+++ libnitrohack/src/makemon.c
@@ -613,6 +613,7 @@ static void m_initinv(struct monst *mtmp)
 		   until Moloch's Sanctum is rearranged */
 		if (ptr == &mons[PM_CTHULHU]) {
 		    mongets(mtmp, AMULET_OF_YENDOR);
+		    mongets(mtmp, AMULET_OF_REFLECTION);
 		    mongets(mtmp, POT_FULL_HEALING);
 		}
 	    default:
