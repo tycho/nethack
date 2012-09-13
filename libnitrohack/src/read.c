fc8f96f008f857f980c92e60423cf659cf25bb46
Tung Nguyen <tungtn3@gmail.com>
2012-09-13 16:13:56 +1000

    Clear new mem_obj bits when forgetting map
    Specifically, this clears the bits for a pile of multiple stacks of
    objects, and whether the object is a Sokoban prize when a map is cleared
    by amnesia.
    
    This is meant to go with commit 662a119c70 (CS715 color Sokoban prizes
    bright magenta) and commit 644c88b3e7 (CS377,513 multiple items on floor
    in inverse video).

--- libnitrohack/src/read.c
+++ libnitrohack/src/read.c
@@ -693,6 +693,8 @@ static void forget_map(struct level *lev, boolean forget_all)
 		lev->locations[zx][zy].mem_trap = 0;
 		lev->locations[zx][zy].mem_obj = 0;
 		lev->locations[zx][zy].mem_obj_mn = 0;
+		lev->locations[zx][zy].mem_obj_stacks = 0;
+		lev->locations[zx][zy].mem_obj_soko = 0;
 		lev->locations[zx][zy].mem_invis = 0;
 		lev->locations[zx][zy].mem_stepped = 0;
 	    }
