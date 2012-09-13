9488c3d604f1a08a36873a0331f5a9221e5c71fb
Tung Nguyen <tungtn3@gmail.com>
2012-09-13 16:12:06 +1000

    CS766: Autoexplore: enable for amnesia'd levels
    Without this, autoexplore will claim that it's done when revisiting an
    amnesia-afflicted level that was completely autoexplored before the
    amnesia.

--- libnitrohack/src/read.c
+++ libnitrohack/src/read.c
@@ -694,6 +694,7 @@ static void forget_map(struct level *lev, boolean forget_all)
 		lev->locations[zx][zy].mem_obj = 0;
 		lev->locations[zx][zy].mem_obj_mn = 0;
 		lev->locations[zx][zy].mem_invis = 0;
+		lev->locations[zx][zy].mem_stepped = 0;
 	    }
 }
 
