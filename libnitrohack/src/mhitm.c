4176ec94843e6bf3c63b43f8c42cf93d5f3116c7
Tung Nguyen <tungtn3@gmail.com>
2012-10-15 20:20:37 +1100

    CS876: Add Grudge patch (by Nephi)
    Original patch here:
    
        http://bilious.alt.org/?62
    
    Compared to UnNetHack (and the original patch), the message for monsters
    hit by the Oracle's magic missiles is "caught in a hail of magic
    missiles" instead of "is hit by magic missiles appearing from thin air",
    which was really awkward.

--- libnitrohack/src/mhitm.c
+++ libnitrohack/src/mhitm.c
@@ -1454,6 +1454,26 @@ static int passivemm(struct monst *magr, struct monst *mdef,
 		    }
 		} else tmp = 0;
 		goto assess_dmg;
+	    case AD_MAGM:
+		/* wrath of gods for attacking Oracle */
+		if (resists_magm(magr)) {
+		    if (canseemon(level, magr)) {
+			shieldeff(magr->mx, magr->my);
+			pline("A hail of magic missiles narrowly misses %s!",
+			      mon_nam(magr));
+		    }
+		} else {
+		    if (canseemon(level, magr)) {
+			if (magr->data == &mons[PM_WOODCHUCK]) {
+			    pline("ZOT!");
+			} else {
+			    pline("%s is caught in a hail of magic missiles!",
+				  Monnam(magr));
+			}
+		    }
+		    goto assess_dmg;
+		}
+		break;
 	    case AD_ENCH:	/* KMH -- remove enchantment (disenchanter) */
 		if (mhit && !mdef->mcan && otmp) {
 		    drain_item(otmp);
