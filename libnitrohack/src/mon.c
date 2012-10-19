2f9436cd8207873729a0e86615c102ee710198a4
Tung Nguyen <tungtn3@gmail.com>
2012-10-19 15:29:21 +1100

    Fix sight-blocking geno'd mimics not unblocking
    Without this fix, giant mimics that take forms that block line-of-sight
    (e.g. boulders, closed doors) will continue to do so even after being
    genocided.
    
    The easiest way to test this is in wizard mode: level teleport to a lit
    version of Big Room, create a dozen giant mimics (some will look like
    boulders) then wish for and read a scroll of genocide.  The points the
    boulder-looking mimics occupied will still block sight to monsters,
    which is very obvious with the dark_room patch.
    
    This bug also exists in vanilla NetHack.

--- libnitrohack/src/mon.c
+++ libnitrohack/src/mon.c
@@ -1310,6 +1310,14 @@ void mondead_helper(struct monst *mtmp, int adtyp)
 	if ((otmp = which_armor(mtmp, W_ARM)) && Is_gold_dragon_armor(otmp->otyp))
 	    end_burn(otmp, FALSE);
 
+	/* monster should no longer block vision */
+	if ((!mtmp->minvis || See_invisible) &&
+	    ((mtmp->m_ap_type == M_AP_FURNITURE &&
+	      (mtmp->mappearance == S_vcdoor || mtmp->mappearance == S_hcdoor)) ||
+	     (mtmp->m_ap_type == M_AP_OBJECT &&
+	      mtmp->mappearance == BOULDER)))
+	    unblock_point(mtmp->mx, mtmp->my);
+
 	mptr = mtmp->data;		/* save this for m_detach() */
 	/* restore chameleon, lycanthropes to true form at death */
 	if (mtmp->cham)
@@ -1480,6 +1488,14 @@ void mongone(struct monst *mdef)
 	/* Player is thrown from his steed when it disappears */
 	if (mdef == u.usteed)
 		dismount_steed(DISMOUNT_GENERIC);
+
+	/* monster should no longer block vision */
+	if ((!mdef->minvis || See_invisible) &&
+	    ((mdef->m_ap_type == M_AP_FURNITURE &&
+	      (mdef->mappearance == S_vcdoor || mdef->mappearance == S_hcdoor)) ||
+	     (mdef->m_ap_type == M_AP_OBJECT &&
+	      mdef->mappearance == BOULDER)))
+	    unblock_point(mdef->mx, mdef->my);
 
 	/* drop special items like the Amulet so that a dismissed Kop or nurse
 	   can't remove them from the game */
