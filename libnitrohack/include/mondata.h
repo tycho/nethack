b027c2c23b60b6bc9af9d19d9e66990e74693a02
Tung Nguyen <tungtn3@gmail.com>
2012-11-18 21:44:17 +1100

    CS1027: Remove anti-matter vortex
    Note that this is not a full revert of the changes that introduced or
    were made to the anti-matter vortex; it only removes the monster itself.
    Code that allows a disintegrating monster to have disintegrating
    engulfing effects still exists, but now it's just unused.

--- libnitrohack/include/mondata.h
+++ libnitrohack/include/mondata.h
@@ -176,8 +176,7 @@
 #define touch_petrifies(ptr)	((ptr) == &mons[PM_COCKATRICE] || \
 				 (ptr) == &mons[PM_CHICKATRICE])
 
-#define touch_disintegrates(ptr) ((ptr) == &mons[PM_DISINTEGRATOR] || \
-				  (ptr) == &mons[PM_ANTI_MATTER_VORTEX])
+#define touch_disintegrates(ptr) ((ptr) == &mons[PM_DISINTEGRATOR])
 
 #define is_mind_flayer(ptr)	((ptr) == &mons[PM_MIND_FLAYER] || \
 				 (ptr) == &mons[PM_MASTER_MIND_FLAYER])
