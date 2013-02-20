c0a9a38abd18295bf6316262ba2f120565d735a8
Tung Nguyen <tungtn3@gmail.com>
2013-02-20 15:01:24 +1100

    Show "Done"/"Never mind" when leaving look modes
    Typing quickly when looking can sometimes make it confusing to know
    whether or not you're still in look mode, so this should help that.

--- libnitrohack/src/pager.c
+++ libnitrohack/src/pager.c
@@ -723,6 +723,8 @@ static int do_look(boolean quick)
 	ans = getpos(&cc, FALSE, what_is_an_unknown_object);
 	if (ans < 0 || cc.x < 0) {
 	    flags.verbose = save_verbose;
+	    if (flags.verbose)
+		pline(quick ? "Never mind." : "Done.");
 	    return 0;	/* done */
 	}
 	flags.verbose = FALSE;	/* only print long question once */
@@ -779,6 +781,8 @@ static int do_look(boolean quick)
     } while (!quick && ans != LOOK_ONCE);
 
     flags.verbose = save_verbose;
+    if (!quick && flags.verbose)
+	pline("Done.");
 
     return 0;
 }
