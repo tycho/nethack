f71440b50978c44587b58c1f2d6c39aedd8474b7
Tung Nguyen <tungtn3@gmail.com>
2013-04-21 21:36:01 +1000

    Fix fleeing monsters getting stuck in corridors
    This fixes the bug where a monster that approaches the player on top of
    an active Elbereth engraving will "turn to flee" and then sit there
    idly for half a dozen turns before actually moving away.
    
    The bug itself is due to the monster movement logic in m_move() having a
    very high chance of ignoring potential moves if they match one of the
    monster's last 4 (MTSZ) positions that are stored in its mtrack field.
    This fix causes monsters to forget those 4 positions at the moment they
    begin to flee so they can do so freely; this fix may have other subtle
    effects, e.g. pets that can't be displaced in shops use fleeing logic to
    move out of the player's way.
    
    In the case of a monster moving towards the player on Elbereth and
    fleeing, the chance that the space they came from being ignored as a
    potential move is rn2(4 * 2), where 4 is a fudge factor and 2 is the
    number of eligible adjacent moves in a straight corridor; the latter
    factor decays for older steps.  The chance to ignore the last 4 stepped
    spaces gets even higher when the monster is out in the open: as high as
    rn2(4 * 8) in fact!
    
    Contrary to popular belief, the monster is NOT trying to move into solid
    stone; mfndpos() does its job and only lists eligible moves.  Also, when
    the player retreats in a corridor with a monster stuck like this, the
    monster moves towards the player NOT to attack them, but because it
    thinks it's the only move it can make.
    
    This bug also exists in vanilla NetHack, upstream NitroHack, UnNetHack
    and NetHack4.

--- libnitrohack/src/monmove.c
+++ libnitrohack/src/monmove.c
@@ -204,6 +204,9 @@ void monflee(struct monst *mtmp, int fleetime, boolean first, boolean fleemsg)
 	    }
 	    mtmp->mflee = 1;
 	}
+
+	/* ignore recently-stepped spaces when made to flee */
+	memset(mtmp->mtrack, MTSZ, sizeof(coord));
 }
 
 static void distfleeck(struct monst *mtmp, int *inrange, int *nearby, int *scared)
@@ -911,6 +914,7 @@ not_special:
 		ny = poss[i].y;
 
 		if (appr != 0) {
+		    /* avoid stepping back onto recently-stepped spaces */
 		    mtrk = &mtmp->mtrack[0];
 		    for (j=0; j < jcnt; mtrk++, j++)
 			if (nx == mtrk->x && ny == mtrk->y)
