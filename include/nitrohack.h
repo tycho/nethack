5f067e7ad29c050b39d69658effce0ec4d656b87
Tung Nguyen <tungtn3@gmail.com>
2013-10-07 19:45:40 +1100

    Bump patchlevel to 0.5.2
    This release greatly increases save file integrity and ability to
    recover by fixing cases of incomplete data being logged that were
    overlooked by NitroHack and NetHack4.
    
    This version is save-compatible with v0.5.0 and v0.5.1, and players of
    these versions are urged to update.  Save files for games that fail to
    recover automatically can be sent to tungtn3+dynahack@gmail.com for
    manual recovery.

--- include/nitrohack.h
+++ include/nitrohack.h
@@ -7,10 +7,10 @@
 #ifndef NITROHACK_H
 #define NITROHACK_H
 
-/* DynaHack 0.5.1 */
+/* DynaHack 0.5.2 */
 #define VERSION_MAJOR	0
 #define VERSION_MINOR	5
-#define PATCHLEVEL	1
+#define PATCHLEVEL	2
 
 #include "nitrohack_types.h"
 
