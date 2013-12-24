7f05cc9ab9864b21f775df2716e3129d5f819812
Tung Nguyen <tungtn3@gmail.com>
2013-12-24 18:49:17 +1100

    Bump patchlevel to 0.5.3
    This release fixes a panic when loading the game with timed objects
    (e.g. rotting corpses, lit fueled light sources) in the Advent Calendar,
    some obscure shop-and-container bugs, #tip command corner cases, and
    introduces the new 'darkmsg' option to control whether past messages are
    darkened.

--- include/nitrohack.h
+++ include/nitrohack.h
@@ -7,10 +7,10 @@
 #ifndef NITROHACK_H
 #define NITROHACK_H
 
-/* DynaHack 0.5.2 */
+/* DynaHack 0.5.3 */
 #define VERSION_MAJOR	0
 #define VERSION_MINOR	5
-#define PATCHLEVEL	2
+#define PATCHLEVEL	3
 
 #include "nitrohack_types.h"
 
