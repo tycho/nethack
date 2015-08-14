01c03f254365b94d96a50cb34c6a00b508b1571a
Tung Nguyen <tungtn3@gmail.com>
2015-08-14 16:02:19 +1000

    Players can wish for up to 100 projectiles (UnNetHackPlus)
    Based on UnNetHack commit c612a6ab66ae9cb1d4a74e2e1ce37cfe31d22489 by
    guest41 (Various minor changes from UnNetHackPlus).

--- libnitrohack/src/objnam.c
+++ libnitrohack/src/objnam.c
@@ -3061,7 +3061,7 @@ typfnd:
 	if (cnt > 0 && objects[typ].oc_merge && oclass != SPBOOK_CLASS &&
 		(cnt < rnd(6) || wizard ||
 		 (cnt <= 7 && Is_candle(otmp)) ||
-		 (cnt <= 20 &&
+		 (cnt <= 100 &&
 		  ((oclass == WEAPON_CLASS && is_ammo(otmp))
 				|| typ == ROCK || is_missile(otmp)))))
 			otmp->quan = (long) cnt;
